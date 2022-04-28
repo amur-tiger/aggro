34c98bc4d918e6e4298214c6738c5ac703979db5
Arne 'TigeR' Linck <github@tiger.rocks>
2022-04-28 17:45:51 +0200

    Added session service

---
+++ packages/server/src/service/session/session-service.ts
@@ -0,0 +1,36 @@
+import { randomUUID } from "crypto";
+import { Service } from "typedi";
+import { DataSource, Repository } from "typeorm";
+import { Session } from "./model/session";
+import { User } from "../user/model/user";
+
+@Service()
+export class SessionService {
+  private readonly repository: Repository<Session>;
+
+  constructor(source: DataSource) {
+    this.repository = source.getRepository(Session);
+  }
+
+  public async findSession(id: string): Promise<Session | null> {
+    return this.repository.findOneBy({ id });
+  }
+
+  public async getAllSessions(user: User): Promise<Session[]> {
+    return this.repository
+      .createQueryBuilder("session")
+      .leftJoinAndSelect("session.user", "user")
+      .where("user.id = :id", { id: user.id })
+      .getMany();
+  }
+
+  public async startSession(user: User): Promise<Session> {
+    const session = new Session(randomUUID(), user);
+    await this.repository.insert(session);
+    return session;
+  }
+
+  public async closeSession(session: Session): Promise<void> {
+    await this.repository.delete(session);
+  }
+}
